81026c8efa2da2d4625887d4226eca9a30caebe1
MichielN19 <Michielmitsjol@gmail.com>
2024-12-04 12:05:13 +0100

    second try fixing textarea bug

--- javascript/textareaHeight.js
+++ javascript/textareaHeight.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const textareas = document.querySelectorAll("textarea");
 
   textareas.forEach((textarea) => {
-    textarea.style.height = textarea.scrollHeight + "px";
     textarea.addEventListener("input", (event) => {
       // if there is input then the height style is set to auto + the content
       textarea.style.height = "auto";
