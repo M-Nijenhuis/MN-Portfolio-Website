a443caa72f971343509aec7ca2ea763c959d6e88
MichielN19 <Michielmitsjol@gmail.com>
2024-12-04 11:40:46 +0100

    Added custom select color and tried fixing textarea bug of being too small

--- javascript/textareaHeight.js
+++ javascript/textareaHeight.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const textareas = document.querySelectorAll("textarea");
 
   textareas.forEach((textarea) => {
-    //textarea.style.height = textarea.scrollHeight + "px";
-    textarea.style.height = "80px";
+    textarea.style.height = textarea.scrollHeight + "px";
     textarea.addEventListener("input", (event) => {
       // if there is input then the height style is set to auto + the content
       textarea.style.height = "auto";
